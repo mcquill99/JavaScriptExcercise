7dd810ab5b6b218e250010dceef77f5bbd8273a2
Sean McQuillan <mcquill99@gmail.com>
2020-03-11 00:00:47 -0400

    Added form to add rows, and delete functionality (one at a time)

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
+var total = 0;
+var id = 0;
+
+function removeRows() {
+    $("#creditorTable").find('input:checkbox').each(function(i, el){
+        if($(el).is(':checked')){
+            document.getElementsByTagName("tr")[i].remove();
+        }
+    });
+}
+
 $(document).ready(function(){
+    function createRow(creditor){
+        let newRow = '';
+        newRow += '<tr class="table-info" id="' + id + '">';
+        newRow += '<td><input type="checkbox"/></td>';
+        newRow += '<td>' + creditor.creditorName + '</td>';
+        newRow += '<td>' +  creditor.firstName + '</td>';
+        newRow += '<td>' + creditor.lastName +  '</td>';
+        newRow += '<td>' + creditor.minPaymentPercentage.toFixed(2) +  '</td>';
+        newRow += '<td>' + creditor.balance.toFixed(2) + '</td>';
+        newRow += '</tr>';
+        total += creditor.balance;
+        id++;
+        return newRow;
+    }
     $.getJSON('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json', function( data ) {
         var toDisplay = '';
         $.each( data, function( id, creditorInfo) {
-            toDisplay += '<tr>';
-            toDisplay += '<td>' + creditorInfo.creditorName + '</td>';
-            toDisplay += '<td>' +  creditorInfo.firstName + '</td>';
-            toDisplay += '<td>' + creditorInfo.lastName +  '</td>';
-            toDisplay += '<td>' + creditorInfo.minPaymentPercentage +  '</td>';
-            toDisplay += '<td>' + creditorInfo.balance + '</td>';
-            toDisplay += '</tr>';
+            toDisplay += createRow(creditorInfo);
         });
 
         $('#creditorTable').append(toDisplay)
     });
+
+    $( "#submit").click(function() {
+        var toDisplay = '';
+        var formdata = $('#add-form').serializeArray();
+        var data = {};
+        $(formdata).each(function(index, obj){
+            data[obj.name] = obj.value;
+        });
+        toDisplay += createRow(data);
+        $('#creditorTable').append(toDisplay)
+    });
+
 });
