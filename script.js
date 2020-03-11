8efe99faa19127c3cdaceaa57f32516ddd9e4eca
Sean McQuillan <mcquill99@gmail.com>
2020-03-11 17:22:47 -0400

    Added total row count in page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var total = 0;
 var id = 0;
+var totalRows = 0;
+var checkedRows = 0;
 
 function calculateTotal(){
     total = 0;
@@ -10,20 +12,28 @@ function calculateTotal(){
     $('#total').text('$' + total.toFixed(2));
 }
 
+function updateRows(){
+    $('#totalRows').text('Total Rows: ' + totalRows);
+}
+
 function removeRows() {
     $("#creditorTable").find('input:checkbox').each(function(i, el){
         if($(el).is(':checked')){
             if(i === 0){
                 $('#creditorTable').find("tr:gt(0)").remove();
                 $(el).prop('checked', false);
+                totalRows = 0;
+                return false; //break
             }
             else{
                 $(el).parents("tr").remove();
+                totalRows--;
 
             }
         }
     });
     calculateTotal();
+    updateRows();
 }
 
 function checkAll(topCheck){
@@ -36,20 +46,24 @@ function checkAll(topCheck){
     });
 }
 
+function createRow(creditor){
+    let newRow = '';
+    newRow += '<tr class="table-info" id="' + id + '">';
+    newRow += '<td><input type="checkbox"/></td>';
+    newRow += '<td>' + creditor.creditorName + '</td>';
+    newRow += '<td>' +  creditor.firstName + '</td>';
+    newRow += '<td>' + creditor.lastName +  '</td>';
+    newRow += '<td>' + parseFloat(creditor.minPaymentPercentage).toFixed(2) +  '</td>';
+    newRow += '<td class="balance">' + parseFloat(creditor.balance).toFixed(2) + '</td>';
+    newRow += '</tr>';
+
+    total += parseFloat(creditor.balance);
+    totalRows++;
+    return newRow;
+}
+
 $(document).ready(function(){
-    function createRow(creditor){
-        let newRow = '';
-        newRow += '<tr class="table-info" id="' + id + '">';
-        newRow += '<td><input type="checkbox"/></td>';
-        newRow += '<td>' + creditor.creditorName + '</td>';
-        newRow += '<td>' +  creditor.firstName + '</td>';
-        newRow += '<td>' + creditor.lastName +  '</td>';
-        newRow += '<td>' + parseFloat(creditor.minPaymentPercentage).toFixed(2) +  '</td>';
-        newRow += '<td class="balance">' + parseFloat(creditor.balance).toFixed(2) + '</td>';
-        newRow += '</tr>';
-        total += parseFloat(creditor.balance);
-        return newRow;
-    }
+
     $.getJSON('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json', function( data ) {
         var toDisplay = '';
         $.each( data, function( id, creditorInfo) {
@@ -58,18 +72,20 @@ $(document).ready(function(){
 
         $('#creditorTable').append(toDisplay);
         calculateTotal();
+        updateRows();
     });
 
     $( "#submit").click(function() {
         var toDisplay = '';
-        var formdata = $('#add-form').serializeArray();
-        var data = {};
-        $(formdata).each(function(index, obj){
-            data[obj.name] = obj.value;
+        var formData = $('#add-form').serializeArray();
+        var parsedData = {};
+        $(formData).each(function(index, obj){
+            parsedData[obj.name] = obj.value;
         });
-        toDisplay += createRow(data);
+        toDisplay += createRow(parsedData);
         $('#creditorTable').append(toDisplay);
         calculateTotal();
+        updateRows();
     });
 
 });
