43cd2431e72872b7a989f39b9267adf4278364f1
Sean McQuillan <mcquill99@gmail.com>
2020-03-12 00:09:42 -0400

    added checkedRows tracker

--- script.js
+++ script.js
@@ -16,6 +16,21 @@ function updateRows(){
     $('#totalRows').text('Total Rows: ' + totalRows);
 }
 
+function updateCheckedRows(){
+    $('#checkedRows').text('Checked Rows: ' + checkedRows);
+}
+
+function incCheckedRows(el){
+    if($(el).is(':checked')){
+        checkedRows++;
+    }
+    else{
+        checkedRows--;
+    }
+
+    updateCheckedRows();
+}
+
 function removeRows() {
     $("#creditorTable").find('input:checkbox').each(function(i, el){
         if($(el).is(':checked')){
@@ -23,33 +38,42 @@ function removeRows() {
                 $('#creditorTable').find("tr:gt(0)").remove();
                 $(el).prop('checked', false);
                 totalRows = 0;
+                checkedRows = 0;
                 return false; //break
             }
             else{
                 $(el).parents("tr").remove();
                 totalRows--;
+                checkedRows--;
 
             }
         }
     });
     calculateTotal();
     updateRows();
+    updateCheckedRows();
 }
 
 function checkAll(topCheck){
     let checked = false;
     if(topCheck.checked){
         checked = true;
+        checkedRows = totalRows;
+    }
+    else{
+        checkedRows = 0;
     }
     $("#creditorTable").find('input:checkbox').each(function(i, el){
         $(el).prop('checked', checked);
     });
+
+    updateCheckedRows();
 }
 
 function createRow(creditor){
     let newRow = '';
     newRow += '<tr class="table-info" id="' + id + '">';
-    newRow += '<td><input type="checkbox"/></td>';
+    newRow += '<td><input type="checkbox" onclick="incCheckedRows(this)"/></td>';
     newRow += '<td>' + creditor.creditorName + '</td>';
     newRow += '<td>' +  creditor.firstName + '</td>';
     newRow += '<td>' + creditor.lastName +  '</td>';
@@ -71,21 +95,32 @@ $(document).ready(function(){
         });
 
         $('#creditorTable').append(toDisplay);
+
         calculateTotal();
         updateRows();
     });
 
+    $("#add").click(function(){
+        $('#add-form').toggle();
+    });
+
     $( "#submit").click(function() {
+        let form = $('#add-form');
         var toDisplay = '';
-        var formData = $('#add-form').serializeArray();
+        var formData = form.serializeArray();
         var parsedData = {};
         $(formData).each(function(index, obj){
             parsedData[obj.name] = obj.value;
         });
+
         toDisplay += createRow(parsedData);
         $('#creditorTable').append(toDisplay);
+
         calculateTotal();
         updateRows();
+
+        form.trigger("reset");
+        form.toggle();
     });
 
 });
